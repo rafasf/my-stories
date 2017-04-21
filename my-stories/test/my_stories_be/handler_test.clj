64817c620e90150c09ef56960e2106907dacb68b
Rafael Ferreira <rafaelsf@gmail.com>
2017-04-21 10:21:23 +0200

    api(test): add proper expectation on handler test

--- my-stories/test/my_stories_be/handler_test.clj
+++ my-stories/test/my_stories_be/handler_test.clj
@@ -7,7 +7,7 @@
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= (:body response) "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n  <meta charset=\"UTF-8\">\n  <title>Stories</title>\n  <link rel=\"stylesheet\" href=\"styles.css\">\n</head>\n<body>\n  <div></div>\n  <script src=\"app.js\"></script>\n  <script>Elm.App.embed(document.querySelector(\"div\"));</script>\n</body>\n</html>\n"))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
